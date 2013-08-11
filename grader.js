aa718283091fa5fbd170e8cd4c7ac67a0cfec4c4
Ubuntu <ubuntu@ip-172-31-41-95.us-west-2.compute.internal>
2013-08-11 00:47:51 +0000

    Edited product desc in index.html

--- grader.js
+++ grader.js
@@ -22,6 +22,7 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('./restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
@@ -31,7 +32,8 @@ var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+        process.exit(1); 
+// http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
@@ -62,14 +64,27 @@ var clone = function(fn) {
 };
 
 if(require.main == module) {
-    console.log(process.argv);
+//    console.log(process.argv);
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url to download>','url info')
         .parse(process.argv);
+        console.log("The URL to be downloaded is " +program.url);
+        if(program.url){
+	console.log('about to download the url');
+	 rest.get(program.url).on('complete',function(result){
+//	     console.log(result);
+	     console.log('download successful');
+	     var checkJson = checkHtmlFile('downFile.html',program.checks);
+	     var outJson = JSON.stringify(checkJson,null,4);
+	     console.log(outJson);
+	 });
+  }else if(program.file){
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+}
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
